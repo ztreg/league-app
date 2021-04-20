48abf014893df4ae2816624fd9250e7de890daef
Jonas Gertz <Jonas.Gertz@hotmail.com>
2021-04-20 11:29:48 +0200

    backup id

--- league-client-v2/src/app/app.component.ts
+++ league-client-v2/src/app/app.component.ts
@@ -10,7 +10,7 @@ import { StoreService } from './services/store.service'
 export class AppComponent implements OnInit{
   constructor(private utils: RequestUtilities, private store: StoreService) {}
   currentUserAccountId!: string
-
+  testID = "22UXnMIItBvFoYv_SJ-O_QnV6GBGPlFu5q-Lu4ZcW9lD1uNqs69xW4Q_"
   ngOnInit(): void {
     this.store.updateCurrentUser({accountId: 'UJhJTXVRisEi4S2ASXmhUmDEYhWJIBfPSmMbQdhAfbM'})
     this.store.currentUser$.subscribe(res => {
