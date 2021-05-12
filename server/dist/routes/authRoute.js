7252c11b5e5c8027407aa87f8b4384510633cd7b
Jonas Gertz <Jonas.Gertz@hotmail.com>
2021-05-12 21:22:45 +0200

    test

--- server/dist/routes/authRoute.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authcontroller_1 = require("../controllers/authcontroller");
-const authRouter = express_1.Router();
-authRouter.post('/login', authcontroller_1.loginController);
-// usersRouter.get('/', getUsers);
-exports.default = authRouter;
--- server/dist/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const usercontroller_1 = require("../controllers/usercontroller");
-const express_1 = require("express");
-const usersRouter = express_1.Router();
-usersRouter.post('/', usercontroller_1.addUser);
-usersRouter.get('/', usercontroller_1.getUsers);
-usersRouter.patch('/:id', usercontroller_1.updateUserFollow);
-exports.default = usersRouter;
