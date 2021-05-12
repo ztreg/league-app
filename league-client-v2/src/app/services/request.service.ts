e8f69883428b0de422de5dcfde8d3872f0f57a88
Jonas Gertz <Jonas.Gertz@hotmail.com>
2021-05-12 22:11:48 +0200

    test8

--- league-client-v2/src/app/services/request.service.ts
+++ league-client-v2/src/app/services/request.service.ts
@@ -12,14 +12,12 @@ export class RequestService {
   itemImageUrl = 'https://ddragon.leagueoflegends.com/cdn/11.7.1/img/item/'
   championsURL = 'https://ddragon.leagueoflegends.com/cdn/11.7.1/data/en_US/champion.json'
   summonersURL = 'https://ddragon.leagueoflegends.com/cdn/11.8.1/data/en_US/summoner.json'
+  // matches = 'lol/match/v4/matches'
 
-  queryToken = 'api_key=RGAPI-44adcb7d-f9df-44c6-bd2b-0b6751773c45'
-  matches = 'lol/match/v4/matches'
-
-  allMatches = 'lol/match/v4/matchlists/by-account'
-  userByNameURL = 'lol/summoner/v4/summoners/by-name'
-  userByIdUrl = '/lol/summoner/v4/summoners/by-account'
-  rankedInfoBySummonerIdURL = 'lol/league/v4/entries/by-summoner'
+  // allMatches = 'lol/match/v4/matchlists/by-account'
+  // userByNameURL = 'lol/summoner/v4/summoners/by-name'
+  // userByIdUrl = '/lol/summoner/v4/summoners/by-account'
+  // rankedInfoBySummonerIdURL = 'lol/league/v4/entries/by-summoner'
 
   constructor(private http: HttpClient) { }
 
